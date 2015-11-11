e29db0e833810981de21db6d683e02503fe03615
Bart <bartland85@o2.pl>
2015-11-11 22:18:15 +0100

    added some marking and font

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
-<p>moja fajna stronka</p>
+<html>
+    <head>
+        <title>Bart's website</title>
+        <style>
+            body{font-family: "Courier New"}
+        </style>
+    </head>
+    <body>
+        <p>moja fajna stronka</p>
 
-<p>Bart</p>
+        <p>Bart</p>
+    </body>
+</html>
